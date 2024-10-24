3ef3f7c0a9e0b218398302baa685fb0e6fa93989
nithishk2001 <168214137+nithishk2001@users.noreply.github.com>
2024-10-24 21:21:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,28 +6,9 @@ const port = process.env.PORT || 5000
 
 
 // -------cors---------- //
-import cors from 'cors';
+import cors from 'cors'
 
-const allowedOrigins = [
-  'https://vcc-front.onrender.com',  // Your main frontend deployment
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    // Check if the request's origin is in the allowed origins
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,  // Allow credentials (cookies, authentication)
-};
-
-app.use(cors(corsOptions));
+app.use(cors({https://vcc-front.onrender.com}))
 
 
 app.use(express.json())
