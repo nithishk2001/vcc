d35950376040d72dc1b3272f371033deba74f011
nithishk2001 <168214137+nithishk2001@users.noreply.github.com>
2024-10-24 21:36:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,7 @@ const app = express()
 const cors = require("cors")
 require("dotenv").config()
 const port = process.env.PORT || 5000
-
-
-// -------cors---------- //
-import cors from 'cors'
-
-app.use(cors({https://vcc-front.onrender.com}))
-
-
+app.use(cors())
 app.use(express.json())
 app.use(require("./routes/record"))
 const dbo = require("./db/conn")
